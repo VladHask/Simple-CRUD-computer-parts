d56f08eec52d51cd49fc12efb518f5feb9f7ec9a
VladHask <41804313+VladHask@users.noreply.github.com>
2019-01-24 17:10:18 +0300

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,24 @@
+
+
+INSERT INTO `part` VALUES
+(1,2,_binary '','processor'),
+(2,3,_binary '','motherboard'),
+(3,2,_binary '\0','sound card'),
+(4,4,_binary '\0','CPU'),
+(5,1,_binary '\0','HDD'),
+(6,10,_binary '','SSD'),
+(7,10,_binary '','Graphic card'),
+(8,15,_binary '','SSD диск'),
+(9,10,_binary '\0','Keyboard'),
+(10,12,_binary '\0','Display'),
+(11,4,_binary '\0','Printer'),
+(12,5,_binary '','Speakers'),
+(13,6,_binary '\0','Mouse'),
+(14,9,_binary '','Optic disk'),
+(15,7,_binary '\0','Webcam'),
+(16,6,_binary '','Intel Core i5'),
+(17,9,_binary '','Intel Core i7'),
+(18,8,_binary '','Intel Core i9'),
+(19,5,_binary '','Intel Core i3 '),
+(20,20,_binary '','AMD'),
+(21,9,_binary '','Celeron');
