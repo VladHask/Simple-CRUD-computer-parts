bfb18212908b26ce469d042b75a37c07122e819a
VladHask <41804313+VladHask@users.noreply.github.com>
2019-01-24 17:17:50 +0300

    Update table.sql

--- table.sql
+++ table.sql
@@ -2,8 +2,9 @@ DROP TABLE IF EXISTS `part`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `part` (
   `id` bigint(20) NOT NULL,
-  `amount` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL AUTO_INCREMENT,
   `must` bit(1) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
